78de8182206cbcaea920c865ed95a588345e33cd
zhixionglou <zhixionglou@gmail.com>
2023-04-22 18:39:32 +0800

    func(service): 新增测试能力

--- src/test/java/com/lzx/learn/systemdesign/SystemDesignApplicationTests.java
+++ src/test/java/com/lzx/learn/systemdesign/SystemDesignApplicationTests.java
@@ -2,12 +2,21 @@ package com.lzx.learn.systemdesign;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class SystemDesignApplicationTests {
 
+    @Resource
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+
+        int count = applicationContext.getBeanDefinitionCount();
+        System.out.println("所有bean的数量为：" + count);
     }
 
 }
